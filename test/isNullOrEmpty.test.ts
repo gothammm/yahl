885a256224090a704803f7f8b414d61ae00fae8a
Gautham Ramachandran <gautham2r@gmail.com>
2020-08-05 23:08:47 +0200

    Fix empty strings for isNullOrEmpty

--- test/isNullOrEmpty.test.ts
+++ test/isNullOrEmpty.test.ts
@@ -3,9 +3,18 @@ import isNullOrEmpty from '../src/isNullOrEmpty';
 
 
 
-test(`should return true for null / undefined / empty objects, arrays`, t => {
+test(`should return true for happy cases`, t => {
   t.true(isNullOrEmpty({}));
   t.true(isNullOrEmpty(null));
   t.true(isNullOrEmpty(undefined));
   t.true(isNullOrEmpty([]));
+  t.true(isNullOrEmpty(''));
 });
+
+test(`should return false for non-happy cases`, t => {
+  t.false(isNullOrEmpty({ x: 'y' }));
+  t.false(isNullOrEmpty('Hello'));
+  t.false(isNullOrEmpty(0));
+  t.false(isNullOrEmpty([1]));
+});
+
