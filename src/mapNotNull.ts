15289f4b4e23799b236b2c83a8dfda2fad77c8aa
Abdulsattar Mohammed <asattar.md@gmail.com>
2020-09-03 23:28:32 +0530

    Optimize mapNotNull

--- src/mapNotNull.ts
+++ src/mapNotNull.ts
@@ -5,7 +5,7 @@ export const mapNotNull = <T, K>(fn: (t: T) => T | K | null, t: T[]): K[] => {
   return t.reduce((acc, item) => {
     const transformed = (fn && fn(item)) ?? null;
     if (isNotNullOrEmpty(transformed)) {
-      return [...acc, transformed] as K[];
+      return acc.concat(transformed) as K[];
     }
     return acc;
   }, [] as K[]);
